db5d4a644f0e7c97a15f5ce76f4bf5152e45d1dd
Angelica Mendez Vega <angelicamendezvega@fabarca.cameiap.local>
2015-07-22 18:39:02 -0500

    Project 0 My app portfolio

---
+++ app/src/main/java/mx/saudade/myappportfolio/MainActivity.java
@@ -0,0 +1,26 @@
+package mx.saudade.myappportfolio;
+
+import android.content.DialogInterface;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String buttonText = ((Button) v).getText().toString();
+        Toast.makeText(this, "This button will launch my " + buttonText, Toast.LENGTH_SHORT).show();
+    }
+
+}
